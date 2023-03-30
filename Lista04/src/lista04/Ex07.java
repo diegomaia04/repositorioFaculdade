4ac7c80e2ce4f52b8b228dde9195e9adcb18faee
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2023-03-30 14:38:16 -0300

    Update Ex07.java

--- Lista04/src/lista04/Ex07.java
+++ Lista04/src/lista04/Ex07.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lista04;
 
-/**
- *
- * @author maiad
- */
+import java.util.Scanner;
+
+
 public class Ex07 {
+
     
-}
+    public static void main(String[] args) {
+        double p,v,np,au,di;
+        Scanner e = new Scanner (System.in);
+        System.out.println("Insira o preço e vendas mensais :\n");
+        p=e.nextDouble();
+        v=e.nextDouble();
+        au=0;
+        di=0;
+        if(p<30&&v<500){
+            au=0.1*p;
+        }else if ((p>=30 && p<80)&&(v>=500 && v<1200)){
+            au=0.15*p;
+        }else{
+            di=0.2*p;
+        }
+        np=p+au-di;
+        System.out.println("novo preço:"+ np);
+        }
+    }
