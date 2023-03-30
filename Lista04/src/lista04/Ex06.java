dcc9b513985583ebb0b57f5a117fcd7e8c2724c9
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2023-03-30 10:04:20 -0300

    Update Ex06.java

--- Lista04/src/lista04/Ex06.java
+++ Lista04/src/lista04/Ex06.java
@@ -4,10 +4,36 @@
  */
 package lista04;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 12523169668
  */
 public class Ex06 {
+    public static void main(String[] args) {
+        Scanner e  = new Scanner(System.in);
+        double s,ns,b,a;
+        System.out.println("Insira o salario");
+        s = e.nextDouble();
+        
+        if (s<500) {
+            b=0.05*s;
+        } else if(s>500 && s< 1200) {
+             b=0.12*s;
+        } else {
+            b=0;
+        }
+        
+        if (s<600) {
+            a=150;
+        } else {
+            a=100;
+        }
+ 
+        //salalrio
+        ns =s+b+a;
+        System.out.println("O valor do Salario será de: " + ns);
+    }
     
 }
