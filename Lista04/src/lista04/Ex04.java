a86e8ae479c28f73ee56ca5eb6975257af4f472b
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2023-03-30 08:00:11 -0300

    Update Ex04.java

--- Lista04/src/lista04/Ex04.java
+++ Lista04/src/lista04/Ex04.java
@@ -4,10 +4,39 @@
  */
 package lista04;
 
+import java.util.Scanner;
+
 /**
  *
  * @author maiad
  */
 public class Ex04 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero, poi;
+
+        System.out.print("Digite numero: ");
+        numero = entrada.nextInt();
+        poi = (numero & 2);
+
+        if (poi == 2) {
+
+            System.out.println("O numero digitado " + numero + " é par");
+            if (numero <= -1) {
+                System.out.println("O numero digitado é negativo");
+            } else {
+                System.out.println("O numero digitado é positivo");
+            }
+        } else {
+            System.out.println("O numero digitado " + numero + " é ímpar");
+            if (numero <= -1) {
+                System.out.println("O numero digitado é negativo");
+            } else {
+                System.out.println("O numero digitado é positivo");
+            }
+
+        }
+
+    }
 }
