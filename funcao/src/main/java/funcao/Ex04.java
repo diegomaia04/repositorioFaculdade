9b6127f93c013118862be680a8a3bff461d29cd7
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2023-04-29 16:25:50 -0300

    Add files via upload

---
+++ funcao/src/main/java/funcao/Ex04.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package funcao;
+
+/**
+ *
+ * @author maiad
+ */
+public class Ex04 {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static Object ehPrimo(int n1){
+        
+        return null;
+        
+    }
+    
+}
