e07e7a0b0c608c3c3c92f2a562a6af1eae8cc7be
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2023-11-14 14:27:44 -0300

    Add files via upload

---
+++ Curso_Extensao/codigo_Pet.WebApp/pages/index.js
@@ -0,0 +1,13 @@
+import GridSection from "@/components/GridSection";
+import HeroMainCard from "@/components/HeroMainCard";
+
+const App = () => {
+    return (
+        <>
+            <HeroMainCard />
+            <GridSection />
+        </>
+    );
+}
+
+export default App
