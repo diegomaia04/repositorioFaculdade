27aa7f85b7466ff65b16cf7513b9fdeffc692a4e
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2024-03-05 14:19:34 -0300

    Add files via upload

---
+++ Unidades Curriculares/Usabilidade, Desevolvimento Web, mobile e jogos/Professor Leandro/VanillaJs_2/VanillaJs/main.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.search-button');
+
+button.addEventListener('click', handleButtonClick);
+
+function handleButtonClick() {
+    alert('The button was clicked');
+}
