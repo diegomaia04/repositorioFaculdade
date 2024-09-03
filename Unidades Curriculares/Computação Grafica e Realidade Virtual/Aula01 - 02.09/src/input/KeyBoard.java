f684894fbf8e6e0fdbd921e1e66d0bcd1fb25838
Diego Maia Fernandes <93396521+diegomaia04@users.noreply.github.com>
2024-09-02 21:18:10 -0300

    Update KeyBoard.java

--- Unidades Curriculares/Computação Grafica e Realidade Virtual/Aula01 - 02.09/src/input/KeyBoard.java
+++ Unidades Curriculares/Computação Grafica e Realidade Virtual/Aula01 - 02.09/src/input/KeyBoard.java
@@ -16,7 +16,20 @@ public class KeyBoard implements KeyListener{
     @Override
     public void keyPressed(KeyEvent e) {        
         System.out.println("Key pressed: " + e.getKeyCode());
+        if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
+            System.exit(0);
         
+        if(e.getKeyCode() == KeyEvent.VK_UP)
+            cena.rotacaoX-=5;
+        
+        if(e.getKeyCode() == KeyEvent.VK_DOWN)
+            cena.rotacaoX+=5;
+        
+        if(e.getKeyCode() == KeyEvent.VK_LEFT)
+            cena.rotacaoY-=5;
+        
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+            cena.rotacaoY+=5;
     }
 
     @Override
